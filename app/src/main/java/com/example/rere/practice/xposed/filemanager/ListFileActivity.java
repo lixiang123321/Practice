37094b2467f2794dd2a0b5d92c1348971f046d4f
rere <xil654321@gmail.com>
2018-07-06 16:07:20 +0800

    1. optimate listFile activity.

--- app/src/main/java/com/example/rere/practice/xposed/filemanager/ListFileActivity.java
+++ app/src/main/java/com/example/rere/practice/xposed/filemanager/ListFileActivity.java
@@ -1,15 +1,18 @@
 package com.example.rere.practice.xposed.filemanager;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.rere.practice.R;
 import com.example.rere.practice.base.activity.BaseActivity;
+import com.example.rere.practice.base.utils.TagLog;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -22,11 +25,17 @@ import java.util.List;
 
 public class ListFileActivity extends BaseActivity {
 
-    private String path;
+    private static final String KEY_PATH = "mPath";
 
+    private String mPath;
     private ListView mListView;
     private ArrayAdapter mListViewAdapter;
 
+    public static void start(Context context, String path) {
+        Bundle bundle = new Bundle();
+        bundle.putString(KEY_PATH, path);
+        context.startActivity(new Intent(context, ListFileActivity.class).putExtras(bundle));
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,15 +51,16 @@ public class ListFileActivity extends BaseActivity {
 
     private void handleData() {
         // Use the current directory as title
-        path = "/";
-        if (getIntent().hasExtra("path")) {
-            path = getIntent().getStringExtra("path");
+        mPath = "/";
+        if (getIntent().hasExtra(KEY_PATH)) {
+            this.mPath = getIntent().getStringExtra(KEY_PATH);
         }
-        setTitle(path);
+        TagLog.i(TAG, "handleData() : " + " mPath = " + mPath + ",");
+        setTitle(this.mPath);
 
         // Read all files sorted into the values-array
         List fileNameStrList = new ArrayList();
-        File pathDir = new File(path);
+        File pathDir = new File(this.mPath);
         if (!pathDir.canRead()) {
             setTitle(getTitle() + " (inaccessible)");
         }
@@ -78,17 +88,25 @@ public class ListFileActivity extends BaseActivity {
 
     protected void onListItemClick(AdapterView<?> l, View v, int position, long id) {
         String filename = (String) mListViewAdapter.getItem(position);
-        if (path.endsWith(File.separator)) {
-            filename = path + filename;
+        if (mPath.endsWith(File.separator)) {
+            filename = mPath + filename;
         } else {
-            filename = path + File.separator + filename;
+            filename = mPath + File.separator + filename;
         }
         if (new File(filename).isDirectory()) {
             Intent intent = new Intent(this, ListFileActivity.class);
-            intent.putExtra("path", filename);
+            intent.putExtra("mPath", filename);
             startActivity(intent);
         } else {
             Toast.makeText(this, filename + " is not a directory", Toast.LENGTH_LONG).show();
         }
     }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        super.setTitle(title);
+        ((TextView) findViewById(R.id.tv_title)).setText(title);
+    }
+
+    
 }
--- app/src/main/java/com/example/rere/practice/xposed/test/TestXposedActs.java
+++ app/src/main/java/com/example/rere/practice/xposed/test/TestXposedActs.java
@@ -6,6 +6,8 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 import com.example.rere.practice.base.activity.TestBaseActivity;
+import com.example.rere.practice.xposed.XposedConstants;
+import com.example.rere.practice.xposed.filemanager.ListFileActivity;
 
 /**
  * Created by rere on 18-7-6.
@@ -40,6 +42,15 @@ public class TestXposedActs extends TestBaseActivity {
             public void onClick(View v) {
                 // test in virtual Xposed environment
                 TestInVirtualXposedEnvironment.start(mContext);
+            }
+        });
+
+        getButton(layout, "file manager", new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // file manager
+                String fileName = XposedConstants.KEY_PREFIX_DATA_USER_0 + XposedConstants.KEY_EXPOSED_VIRTUAL;
+                ListFileActivity.start(mContext, fileName);
             }
         });
     }
